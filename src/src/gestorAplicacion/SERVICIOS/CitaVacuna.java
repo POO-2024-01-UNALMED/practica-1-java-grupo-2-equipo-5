c9fc10a576cbfe2e5cd9f21cf9dc3095b41bdcdc
Manuelmera <157912912+Manuelmera@users.noreply.github.com>
2024-08-07 11:36:55 -0500

    Create CitaVacuna.java

---
+++ src/src/gestorAplicacion/SERVICIOS/CitaVacuna.java
@@ -0,0 +1,42 @@
+package gestorAplicacion.SERVICIOS;
+import gestorAplicacion.AdministracionHospital.Vacuna;
+import gestorAplicacion.Persona.Paciente;
+
+public class CitaVacuna extends Cita {
+    private Vacuna vacuna;
+    #Atributos
+
+    public CitaVacuna( Vacuna vacuna, String fecha, Paciente paciente){
+        super(null, fecha, paciente)
+        this.vacuna=vacuna;
+    }
+    #Constructor
+
+    @Override
+    public void validarPago(Paciente paciente, long idServicio){
+        for (CitaVacuna citaVacuna:  paciente.getHistoriaClinica().getHistorialCitas()){
+            if(citaVacuna.getIdServicio() == idServicio){
+                citaVacuna.getEstadoPago(true);
+            }
+        }
+    }
+    @Override
+    public String descripcionServicio() {
+        return idServicio + " - Vacuna " + vacuna.getNombre() + " (" + fecha + ")";
+    }
+    #metodos
+
+    public Vacuna getVacuna() {
+        return vacuna;
+    }
+    public void setVacuna(Vacuna vacuna) {
+        this.vacuna = vacuna;
+    }
+  
+    public String mensaje(){
+        return "del servicio de vacunas";
+    }
+    #gets y sets
+    
+
+}
