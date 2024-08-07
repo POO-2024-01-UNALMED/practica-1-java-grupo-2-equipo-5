cd2b8118f684dc978517a70ae84c2a1e92e5f8d9
SamuelRamirezM <sramirezmu@unal.edu.co>
2024-08-07 16:18:38 -0500

    Revision problemas importacion

--- src/src/gestorAplicacion/AdministracionHospital/HistoriaClinica.java
+++ src/src/gestorAplicacion/AdministracionHospital/HistoriaClinica.java
@@ -1,10 +1,18 @@
-package src.gestorAplicacion.AdministracionHospital;
+package src.gestorAplicacion.administracionHospital;
 
+import java.util.ArrayList;
+import gestorAplicacion.SERVICIOS.Cita;
+import gestorAplicacion.Persona.Paciente;
 //Falta importar las demás clases aquí...
 
-//Creamos las historias clínicas de los pacientes.
+//Creamos las historias clínicas de los pacientes:
 //Podriamos hacer que esto sea un método?
-public class HistoriaClinica {
+
+public class HistoriaClinica { // ->Implementar la interface Serializable
+
     //Atributos.
+
     private final Paciente paciente; //Único paciente asociado (final).
+    private ArrayList<Formula> listaFormulas = new ArrayList<>();
+    private ArrayList<Cita> historialCitas = new ArrayList<>();
 }
