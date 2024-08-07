bfd3168a823681a334a0f1453eaa5a88fb59e643
SamuelRamirezM <sramirezmu@unal.edu.co>
2024-08-07 12:21:03 -0500

    Organización de paquete gestionAplicacion

---
+++ src/src/gestorAplicacion/AdministracionHospital/HistoriaClinica.java
@@ -0,0 +1,10 @@
+package src.gestorAplicacion.AdministracionHospital;
+
+//Falta importar las demás clases aquí...
+
+//Creamos las historias clínicas de los pacientes.
+//Podriamos hacer que esto sea un método?
+public class HistoriaClinica {
+    //Atributos.
+    private final Paciente paciente; //Único paciente asociado (final).
+}
