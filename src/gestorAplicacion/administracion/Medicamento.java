e66432bcdd8deb38b523a80df918a9dacc236ee2
jerozapata <159493286+jerozapata@users.noreply.github.com>
2024-08-08 14:34:55 -0500

    Metodos medicamentos

--- src/gestorAplicacion/administracion/Medicamento.java
+++ src/gestorAplicacion/administracion/Medicamento.java
@@ -20,4 +20,53 @@ public class Medicamento implements Serializable {
         this.precio=precio;
     }
 
+    public void eliminarCantidad(){
+        this.cantidad -= 1;
+    }
+
+    public String getNombre(){
+        return nombre
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public Enfermedad getEnfermedad(){
+        return enfermedad;
+    
+    }
+
+    public void setEnfermedad(){
+        this.enfermedad = enfermedad;
+    }
+
+    public String getDescripcion(){
+        return descripcion;
+    }
+
+    public void setDescripcion(){
+        this.descripcion = descripcion;
+
+    }
+
+    public int getcantidad(){
+        return cantidad;
+    }
+
+    public void setCantidad( int cantidad){
+        this.cantidad = cantidad;
+    }
+
+    public float getPrecio(){
+        return precio;
+    }
+
+    public void setPrecio(float precio){
+        this.precio = precio;
+    }
+
+    public String toString(){
+        return "Nombre:" + nombre +", " + "Enfermedad:"+ enfermedad.getNombre()+" "+ enfermedad.getTipologia()+", "+"Descripcion:"+descripcion;
+    }
 }
