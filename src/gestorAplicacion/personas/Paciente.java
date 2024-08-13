2a596c20f8a153fe2fa5830b54498212fe2a11e5
Chocorramosaurus <159493300+PabloJimenez028@users.noreply.github.com>
2024-08-12 21:26:56 -0500

    La cafeína mi pastor nada me faltara

--- src/gestorAplicacion/personas/Paciente.java
+++ src/gestorAplicacion/personas/Paciente.java
@@ -1,17 +1,200 @@
 package gestorAplicacion.personas;
 
-public class Paciente{// extends Persona implements Pago
+import gestorAplicacion.administracionHospital.*;
+import gestorAplicacion.servicios.*;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Paciente extends Persona implements Pago{
 
     //Atributos
-    private final HistoriaClinica hostoriaClinica; //Realizar clase HistoriaClinica
-
-    private CategoriaHabitacion categoriaHabitacion; //Crear clase CatehoriaHabitacion
+    private final HistoriaClinica historiaClinica;
+    private CategoriaHabitacion categoriaHabitacion;
     private Habitacion habitacionAsignada;
 
     //Constructor
 
-    public Paciente(int cedula, String nombre, String tipoEps){
+    public Paciente(int cedula, String nombre, String tipoEps, CategoriaHabitacion categoriaHabitacion) {
+        super(cedula, nombre, tipoEps);
+        this.categoriaHabitacion = categoriaHabitacion;
+        this.historiaClinica = new HistoriaClinica(this);
 
     }
+    //Metodos
 
+
+    //METODO ENCARGADO DE REALIZAR LA BUSQUEDA DE MEDICAMENTOS DISPONIBLES
+    public ArrayList<Medicamento> medEnfermedades(Enfermedad enfermedad, Hospital hospital){
+        ArrayList<Medicamento> medicamentospacientes = new ArrayList<Medicamento>();
+        ArrayList<Medicamento> medEnfermedades = new ArrayList<Medicamento>();
+        for (Medicamento med: medicamentospacientes) {
+            if (enfermedad.getNombre().equals(med.getEnfermedad().getNombre()) && enfermedad.getTipologia().equals(med.getEnfermedad().getTipologia())) {
+                medicamentospacientes.add(med);
+            }
+        }
+        return medEnfermedades;
+    }
+
+    public ArrayList<Doctor> buscarDoctorEps(Doctor doctor, Hospital hospital) {
+        ArrayList<Doctor> doctoresPorEspecialidad = hospital.buscarTipoDoctor(especialidad);
+        ArrayList<Doctor> doctoresDisponibles = new ArrayList<Doctor>();
+
+        for (int i = 1; i <= doctoresPorEspecialidad.size(); i++) {
+            if (Objects.equals(doctoresPorEspecialidad.get(i - 1).getTipoEps(), getTipoEps())) {
+                doctoresDisponibles.add(doctoresPorEspecialidad.get(i - 1));
+            }
+        }
+        return doctoresDisponibles;
+    }
+    public void actualizarHistorialCitas(Cita citaAsignada) {
+        historiaClinica.getHistorialCitas().add(citaAsignada);
+    }
+    public double calcularPrecio(Formula formula) {
+        double precio = 0;
+        for (Medicamento med : formula.getListaMedicamentos()) {
+            if (formula.getPaciente().getTipoEps().equals("Contributivo")) {
+                precio += med.getPrecio() * 0.8;
+            }
+            if (formula.getPaciente().getTipoEps().equals("Subsidiado")) {
+                precio += med.getPrecio() * 0.7;
+            }
+            if (formula.getPaciente().getTipoEps().equals("Particular")) {
+                precio += med.getPrecio();
+            }
+        }
+        return precio * (1 + IVA);
+    }
+    public double calcularPrecio(Cita citaAsignada) {
+
+        double precioTotal = 0;
+
+        if (Objects.equals(citaAsignada.getDoctor().getEspecialidad(),"General")) {
+            precioTotal += 5000;
+        }
+
+        if (Objects.equals(citaAsignada.getDoctor().getEspecialidad(),"Oftalmologia")) {
+            precioTotal += 7000;
+        }
+
+        if (Objects.equals(citaAsignada.getDoctor().getEspecialidad(),"Odontologia")) {
+            precioTotal += 10000;
+        }
+
+        if (Objects.equals(citaAsignada.getPaciente().getTipoEps(),"Contributivo")) {
+            precioTotal += 2000;
+        }
+
+        if (Objects.equals(citaAsignada.getPaciente().getTipoEps(),"Subsidiado")) {
+            precioTotal += 500;
+        }
+
+        if (Objects.equals(citaAsignada.getPaciente().getTipoEps(),"Particular")) {
+            precioTotal += 10000;
+        }
+
+        precioTotal = precioTotal*(1+IVA);
+        return precioTotal;
+    }
+    public double calcularPrecio(CitaVacuna citaAsignada) {
+        double precioTotal = citaAsignada.getVacuna().getPrecio();
+
+        if (Objects.equals(citaAsignada.getVacuna().getTipo(), "Obligatoria")) {
+            precioTotal += 1000;
+        }
+        if (Objects.equals(citaAsignada.getVacuna().getTipo(), "No obligatoria")) {
+            precioTotal += 3000;
+        }
+        if (Objects.equals(citaAsignada.getPaciente().getTipoEps(), "Contributivo")) {
+            precioTotal += 2000;
+        }
+        if (Objects.equals(citaAsignada.getPaciente().getTipoEps(), "Subsidiado")) {
+            precioTotal += 500;
+        }
+        if (Objects.equals(citaAsignada.getPaciente().getTipoEps(), "Particular")) {
+            precioTotal += 10000;
+        }
+        precioTotal = precioTotal * (1 + IVA);
+        return precioTotal;
+    }
+    public  double calcularPrecio(Habitacion habitacionAsignada)
+    {
+        double precio=0;
+        if (Objects.equals(getTipoEps(), "Subsidiado"))
+        {
+            precio=habitacionAsignada.getCategoria().getValor()*0;
+            return precio*(1+IVA);
+        }
+        else if (Objects.equals(getTipoEps(), "Contributivo"))
+        {
+            precio=(habitacionAsignada.getCategoria().getValor()/2)*habitacionAsignada.getDias();
+            return precio*(1+IVA);
+        }
+        else
+        {
+            precio=habitacionAsignada.getCategoria().getValor()*habitacionAsignada.getDias();
+            return precio*(1+IVA);
+        }
+    }
+
+    public ArrayList<Vacuna> buscarVacunaPorEps(String tipo, Hospital hospital){
+        ArrayList<Vacuna> vacunasPorTipo= hospital.buscarTipoVacuna(tipo);
+        ArrayList<Vacuna> vacunasDisponibles= new ArrayList<Vacuna>();
+
+        for (int i=1; i<=vacunasPorTipo.size();i++){
+            ArrayList<String> tipoEpsVacuna = vacunasPorTipo.get(i-1).getTipoEps();
+            for (int j=1;j<=tipoEpsVacuna.size();j++){
+                if (Objects.equals(tipoEpsVacuna.get(j - 1), getTipoEps())){
+                    vacunasDisponibles.add(vacunasPorTipo.get(i-1));
+                }
+            }
+        }
+        return vacunasDisponibles;
+    }
+    public String mensajeDoctor(Persona doctor){
+        //Hay ligadura dinámica
+        return doctor.bienvenida()+ "\nPor favor selecciona los medicamentos que vas a formularle a: "+getNombre();
+    }
+
+    //Getter y setter
+
+    //Agrega una cita de vacuna al historial de vacunas del paciente
+    public void actualizarHistorialVacunas(CitaVacuna citaAsignada){
+        historiaClinica.getHistorialVacunas().add(citaAsignada);
+    }
+
+    public HistoriaClinica getHistoriaClinica() {
+        return historiaClinica;
+    }
+
+    public CategoriaHabitacion getCategoriaHabitacion()
+    {
+        return categoriaHabitacion;
+    }
+
+    public void setCategoriaHabitacion(CategoriaHabitacion categoriaHabitacion) {
+        this.categoriaHabitacion = categoriaHabitacion;
+    }
+    public Habitacion getHabitacionAsignada() {
+        return habitacionAsignada;
+    }
+
+    public void setHabitacionAsignada(Habitacion habitacionAsignada) {
+        this.habitacionAsignada = habitacionAsignada;
+    }
+
+    @Override
+
+    public String toString(){
+        return "---------------------------\nNombre: "+ getNombre() + "\nCédula: " + getCedula() + "\nTipo de EPS: " + getTipoEps() + "\n---------------------------";
+    }
+
+    public String despedida(Cita citaAsignada ){
+        /*Hay ligadura dinámica ya que la referencia
+        citaAsignada puede apuntar a una cita o a una cita vacuna
+        y dependiendo de eso ejecutará el método más específico
+        de mensaje().
+         */
+        return "¡Adiós "+getNombre()+" "+citaAsignada.mensaje();
+    }
 }
