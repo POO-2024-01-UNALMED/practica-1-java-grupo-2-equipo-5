b35636291e6085ae479b34b96bd774a5f62131fd
Manuelmera <157912912+Manuelmera@users.noreply.github.com>
2024-08-13 20:21:40 -0500

    Create AgregarCitas.java

---
+++ src/uiMain/gestion/gestionDoctores/AgregarCitas.java
@@ -0,0 +1,11 @@
+package uiMain.gestion.gestionDoctores;
+
+import gestorAplicacion.administracion.Hospital;
+import gestorAplicacion.personas.Doctor;
+import gestorAplicacion.servicios.Cita;
+
+import java.util.Scanner;
+
+public class AgregarCitas {
+    
+}
