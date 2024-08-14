10b513e8731bb483795f3911063b7d2d5d8e8e7d
jerozapata <159493286+jerozapata@users.noreply.github.com>
2024-08-14 15:10:14 -0500

    Eliminar citaVacuna.java

--- src/uiMain/gestion/gestionVacunas/AgregarElinimarCitaVacuna.java
+++ src/uiMain/gestion/gestionVacunas/AgregarElinimarCitaVacuna.java
@@ -1,5 +1,8 @@
 package uiMain.gestion.gestionVacunas;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import gestorAplicacion.administracionHospital.Hospital;
 import gestorAplicacion.administracionHospital.Vacuna;
 import gestorAplicacion.servicios.CitaVacuna;
@@ -33,4 +36,54 @@ public class AgregarElinimarCitaVacuna {
             }
         }
     }   
+
+
+    public static void eliminarCiatvacuna(Hospital hospital){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese el nombre de la vacuna a la que se le eliminará su cita");
+        String nombreVacuna = sc.nextLine();
+
+        if(!RegistroVacuna.verificarExistenciaVacuna(nombreVacuna, hospital)){
+            System.out.println("Esta vacuna no existe en el inventario del hospital.");
+
+        }
+
+        else{
+            Vacuna vacuna = hospital.buscarVacuna(nombreVacuna);
+            System.out.println("A continuación se muestran las citas de esta vacuna que no tiene paciente asignado: ");
+            ArrayList<CitaVacuna> agendaDisponible = vacuna.mostrarAgendaDisponible();
+
+            if(agendaDisponible.size()==0){
+                System.out.println("No hay citas disponibles para eliminar");
+                return;
+            
+            }
+
+            for(int i=1; i<= agendaDisponible.size(); i++){
+                System.out.println(i+". "+ agendaDisponible.get(i-1).getFecha());
+            }
+
+            System.out.println("Seleccione la cita que desea eliminar: ");
+            byte numeroCita = sc.nextByte();
+
+            while (numeroCita<1 || numeroCita>agendaDisponible.size()) {
+                System.out.println("Ingrese un número válido");
+                numeroCita = sc.nextByte();
+                
+            }
+
+            for(int i=1; i<vacuna.getAgenda().size();i++){
+                if(Objects.equals(vacuna.getAgenda().get(-1).getFecha(), agendaDisponible.get(numeroCita-1).getFecha())){
+                    vacuna.getAgenda().remove(vacuna.getAgenda().get(i-1));
+                }
+            }
+            System.out.println("Cita eliminada con éxito");
+            System.out.println("\nVacuna "+vacuna.getNombre());
+            System.out.println("Agenda: ");
+            for (CitaVacuna agenda : vacuna.getAgenda()){
+                System.out.println(agenda.getFecha());
+            }
+        }
+    }
 }
