c826dcc22f8afb2f7c6ac19a2ce6c690268e598f
jerozapata <159493286+jerozapata@users.noreply.github.com>
2024-08-14 13:52:42 -0500

    Update Pago.java

--- src/gestorAplicacion/administracionHospital/Pago.java
+++ src/gestorAplicacion/administracionHospital/Pago.java
@@ -2,16 +2,16 @@ package gestorAplicacion.administracionHospital;
 
 import gestorAplicacion.servicios.Cita;
 import gestorAplicacion.servicios.CitaVacuna;
-import gestorAplicacion.servicios.Formula;
 import gestorAplicacion.servicios.Habitacion;
+import gestorAplicacion.servicios.Formula;
 
 public interface Pago {
 
     double IVA = 0.19;
 
-    double calcularPrecio(Formula formula);
-    double calcularPrecio(Cita cita);
-    double calcularPrecio(CitaVacuna citaVacuna);
-    double calcularPrecio(Habitacion habitacion);
+    double calcularValor(Formula formula);
+    double calcularValor(Cita cita);
+    double calcularValor(CitaVacuna citaVacuna);
+    double calcularValor(Habitacion habitacion);
 
 }
