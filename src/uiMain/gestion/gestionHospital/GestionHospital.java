dde5bfa6d4a7542ad8efa3fe1f98599cfd35c3f0
SamuelRamirezM <sramirezmu@unal.edu.co>
2024-08-14 23:02:44 -0500

    Impresión de opciones

--- src/uiMain/gestion/gestionHospital/GestionHospital.java
+++ src/uiMain/gestion/gestionHospital/GestionHospital.java
@@ -3,6 +3,7 @@ package uiMain.gestion.gestionHospital;
 import java.util.Scanner;
 
 //import baseDatos.Serializador;
+import baseDatos.Serializador;
 import gestorAplicacion.administracionHospital.Hospital;
 
 
@@ -12,6 +13,23 @@ public class GestionHospital {
     public static void menuGestionHospital(Hospital hospital) {
         byte opcion;
 
-        do {}
+        do {
+            System.out.println("\n---Menu Gestion Hospital---");
+            System.out.println("Por favor, elija una de las siguientes opciones: ");
+            System.out.println("1. Construir Habitación");
+            System.out.println("2. Ver lista de Habitaciones");
+            System.out.println("3. Destruir Habitación");
+            System.out.println("4. Agregar Medicamentos");
+            System.out.println("5. Ver Inventario de medicamentos");
+            System.out.println("6. Ver personas registradas en el hospital");
+            System.out.println("7. Ver vacunas registradas en el hospital");
+            System.out.println("8. --Regresar al menu anterior--");
+            System.out.println("9. --Salir--");
+
+            opcion = sc.nextByte();
+
+            
+            }
+        } while (true);
     }
 }
