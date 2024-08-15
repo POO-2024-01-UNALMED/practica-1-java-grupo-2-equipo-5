2bf31eb8a1bfe8f5e4671d346b5166d03e31dd57
SamuelRamirezM <sramirezmu@unal.edu.co>
2024-08-14 22:38:36 -0500

    Problemas con el println

--- src/uiMain/gestion/gestionHospital/DestruirHbaitacion.java
+++ src/uiMain/gestion/gestionHospital/DestruirHbaitacion.java
@@ -1,4 +1,18 @@
 package uiMain.gestion.gestionHospital;
 
+import java.util.Scanner;
+
+import gestorAplicacion.administracionHospital.CategoriaHabitacion;
+import gestorAplicacion.administracionHospital.Hospital;
+import gestorAplicacion.servicios.Habitacion;
+
 public class DestruirHbaitacion {
+    public static void destruirHabitacion(Hospital hospital) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Por favor, introduzca la información de la habitación que desea destruir");
+        System.out.println("Ingrese el número de la habitación: ");
+        int numero = sc.nextInt();
+        System.out.println("¿Qué tipo de habitación desea destruir?");
+        System.out.println("nada")
+    }
 }
