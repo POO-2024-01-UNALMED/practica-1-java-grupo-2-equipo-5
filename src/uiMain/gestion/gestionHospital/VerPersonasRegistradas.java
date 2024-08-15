22c3b4fdb324736a0f99fdc2c7ea7eccaeb18d1c
Chocorramosaurus <159493300+PabloJimenez028@users.noreply.github.com>
2024-08-14 21:39:45 -0500

    Metodo complementario VerPersonasRegistradas

---
+++ src/uiMain/gestion/gestionHospital/VerPersonasRegistradas.java
@@ -0,0 +1,24 @@
+package uiMain.gestion.gestionHospital;
+
+import gestorAplicacion.administracionHospital.Hospital;
+import gestorAplicacion.personas.Doctor;
+import gestorAplicacion.personas.Paciente;
+
+import java.util.ArrayList;
+
+public class VerPersonasRegistradas {
+    public static void verPersonasRegistradas(Hospital hospital) {
+        ArrayList<Doctor> listaDoctores = hospital.getListaDoctores();
+        ArrayList<Paciente> listaPacientes = hospital.getListaPacientes();
+
+        System.out.println("Doctores: ");
+        for (Doctor doctor : listaDoctores) {
+            System.out.println(doctor);
+        }
+        System.out.println("Pacientes: ");
+        for (Paciente paciente : listaPacientes) {
+            System.out.println(paciente);
+        }
+
+    }
+}
