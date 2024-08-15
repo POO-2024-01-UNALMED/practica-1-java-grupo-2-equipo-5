2997cb216c7bdc635c78d9d0bc992c39aea4d8b4
SamuelRamirezM <sramirezmu@unal.edu.co>
2024-08-14 22:53:00 -0500

    Creación de menú para la gestión

---
+++ src/uiMain/gestion/gestionHospital/GestionHospital.java
@@ -0,0 +1,17 @@
+package uiMain.gestion.gestionHospital;
+
+import java.util.Scanner;
+
+//import baseDatos.Serializador;
+import gestorAplicacion.administracionHospital.Hospital;
+
+
+public class GestionHospital {
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void menuGestionHospital(Hospital hospital) {
+        byte opcion;
+
+        do {}
+    }
+}
