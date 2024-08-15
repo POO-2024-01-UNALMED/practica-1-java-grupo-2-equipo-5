9eacd1c5f33b54b4ef482ac04754f1a6472172cb
SamuelRamirezM <sramirezmu@unal.edu.co>
2024-08-14 22:48:38 -0500

    Ultimas confirmaciones

--- src/uiMain/gestion/gestionHospital/DestruirHbaitacion.java
+++ src/uiMain/gestion/gestionHospital/DestruirHbaitacion.java
@@ -7,6 +7,7 @@ import gestorAplicacion.administracionHospital.Hospital;
 import gestorAplicacion.servicios.Habitacion;
 
 public class DestruirHbaitacion {
+
     public static void destruirHabitacion(Hospital hospital) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Por favor, introduzca la información de la habitación que desea destruir");
@@ -16,10 +17,19 @@ public class DestruirHbaitacion {
         System.out.println("'CAMILLA', 'INDIVIDUAL', 'DOBLE', 'OBSERVACION', 'UCI', 'UCC': "); //Igual que en ConstruirHabitacion?
         String categoriaEscogida = sc.nextLine();
         CategoriaHabitacion hEscogida = CategoriaHabitacion.valueOf(categoriaEscogida);
+
         for (Habitacion habitacion : Hospital.getListaHabitaciones()) {
             if (habitacion.getNumero() == nHabitacion && habitacion.getCategoria() == hEscogida) {
-                if (!habitacion.isOcupada()) {}
+                if (!habitacion.isOcupada()) {
+                    Hospital.getListaHabitaciones().remove(habitacion);
+                    System.out.println("Se ha destruido la habitación!");
+                    return;
+                } else {
+                    System.out.println("La habitación está en uso y no puede ser destruida en este momento.");
+                    return;
+                }
             }
         }
+        System.out.println("La habitación que intenta destruir no existe.");
     }
 }
